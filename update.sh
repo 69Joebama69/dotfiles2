ff3240c78d637fe5c5b44d8101afa2b23be8f2c4
69Joebama69 <hannokofler04@gmail.com>
2022-05-02 11:19:11 +0200

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 path="/home/$USER/.config"
 
 cp /home/$USER/.Xmodmap  /home/$USER/.xinitrc  /home/$USER/.bashrc  /home/$USER/.Xresources .
-cp -r $path/dunst/ $path/fish/ $path/i3/ $path/kitty/ $path/mpv/ $path/nvim/ $path/picom/ $path/polybar/ $path/ranger/ $path/rofi/ $path/wallpapers/ .
+cp -r $path/dunst/ $path/fish/ $path/i3/ $path/kitty/ $path/mpv/ $path/nvim/ $path/picom/ $path/polybar/ $path/ranger/ $path/rofi/ $path/wallpapers/ /home/$USER/projects/dotfiles2/
 
 msg="$1"
 [ -z "$msg" ] && msg="update"
 
+dir=pwd
+
+cd /home/$USER/projects/dotfiles2/
+
 git add .
 git commit -m "$msg"
 git push
+
+cd $dir
