3a60e487d6ae3fb3a4aeeffd80dca8f6fbffee58
69Joebama69 <hannokofler04@gmail.com>
2022-05-02 08:37:48 +0200

    update

--- update.sh
+++ update.sh
@@ -1,2 +1,9 @@
-cd /home/hannes/.config/
-cp dunst/ fish/ i3/ kitty/ mpv/ nvim/ picom/ polybar/ ranger/ rofi/ wallpapers/ ../projects/dotfiles2/ -r
+path="/home/$USER/.config"
+cp -r $path/dunst/ $path/fish/ $path/i3/ $path/kitty/ $path/mpv/ $path/nvim/ $path/picom/ $path/polybar/ $path/ranger/ $path/rofi/ $path/wallpapers/ .
+
+msg="$1"
+[ -z "$msg" ] && msg="update"
+
+git add .
+git commit -m "$msg"
+git push
