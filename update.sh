0572f3cc220cd6506ec539277010f88df34b1577
69Joebama69 <hannokofler04@gmail.com>
2022-06-04 20:31:48 +0200

    update

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ config="$home/.config"
 cp $home/.Xmodmap  $home/.xinitrc  $home/.bashrc  $home/.Xresources .
 cp -r $config/dunst/ $config/fish/ $config/i3/ $config/kitty/ $config/mpv/ $config/nvim/ $config/picom/ $config/polybar/ $config/ranger/ $config/rofi/ $config/wallpapers/ /home/$USER/projects/dotfiles2/
 
+cp /usr/share/backgrounds/the-great-wave2-nord.png /home/$USER/projects/dotfiles2/
+
 msg="$1"
 [ -z "$msg" ] && msg="update"
 
